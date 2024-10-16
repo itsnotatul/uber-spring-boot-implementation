4488b509a479a9c2566c35d4fb2d1c39a113bd17
Atul <atultiwariee60@gmail.com>
2024-10-17 01:45:24 +0530

    add application properties, linking to postgress local instance

---
+++ src/main/java/com/example/UberDemoProject/UberDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberDemoProjectApplication.class, args);
+	}
+
+}
